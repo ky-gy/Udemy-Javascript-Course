b81c69da1c375ba0aa5ef570e7beeaf3703a61b6
Jallele <kyle_anderson2@yahoo.com>
2024-04-28 12:22:30 -0700

    Completed Section 26

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
@@ -16,7 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  // get access to email, password, passwordConfirmation
+  console.log(req.body);
   res.send("Account Created!!");
 });
 
