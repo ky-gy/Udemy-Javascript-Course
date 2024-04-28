1a58a82564d3c16b5e0d711e7fd68037890fb605
Jallele <kyle_anderson2@yahoo.com>
2024-04-28 11:57:32 -0700

    video 357 save

--- ecomm/index.js
+++ ecomm/index.js
@@ -3,7 +3,21 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi there!");
+  res.send(`
+    <div> 
+      <form method="POST"> 
+        <input name="email" placeholder ="email" />
+        <input name="password" placeholder ="password" />
+        <input name="passwordconfirmation" placeholder ="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  // get access to email, password, passwordConfirmation
+  res.send("Account Created!!");
 });
 
 app.listen(3000, () => {
