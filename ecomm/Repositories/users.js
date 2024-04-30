58a86dfffbfe4f222f8fca233a9db5f2a1d21b01
Jallele <kyle_anderson2@yahoo.com>
2024-04-29 21:20:50 -0700

    Finished video 368

---
+++ ecomm/Repositories/users.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a repository requires a filename");
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+
+  async getAll() {
+    // Open the file called this.filename
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8",
+      })
+    );
+  }
+  async create(attrs) {
+    attrs.id = this.randomId();
+
+    // {email: 'asdf@asdf.com', password: 'password' }
+    const records = await this.getAll();
+    records.push(attrs);
+    // Write the updated 'records' array back to 'users.json' aka this.filename
+    await this.writeAll(records);
+  }
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.id === id);
+  }
+}
+
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+  const user = await repo.getOne("bobloblaw");
+
+  console.log(user);
+};
+
+test();
