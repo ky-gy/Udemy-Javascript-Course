80446c8ac71518a1a8ca9ba705cdab04e60ae11b
Jallele <kyle_anderson2@yahoo.com>
2024-04-30 18:37:41 -0700

    Finish section 27

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const usersRepo = require("./Repositories/users");
 
 const app = express();
 
@@ -11,15 +12,26 @@ app.get("/", (req, res) => {
       <form method="POST"> 
         <input name="email" placeholder ="email" />
         <input name="password" placeholder ="password" />
-        <input name="passwordconfirmation" placeholder ="password confirmation" />
+        <input name="passwordConfirmation" placeholder ="password confirmation" />
         <button>Sign Up</button>
       </form>
     </div>
   `);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    console.log(req.body);
+    console.log(password, passwordConfirmation);
+    return res.send("Passwords must match");
+  }
   res.send("Account Created!!");
 });
 
