2610b6ee22ca15f8146805087a46b8ec95dd8b71
Jallele <kyle_anderson2@yahoo.com>
2024-04-30 18:02:50 -0700

    finished 369

--- ecomm/Repositories/users.js
+++ ecomm/Repositories/users.js
@@ -46,13 +46,17 @@ class UsersRepository {
     const records = await this.getAll();
     return records.find((record) => record.id === id);
   }
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  const user = await repo.getOne("bobloblaw");
 
-  console.log(user);
+  await repo.delete("e52959ca");
 };
 
 test();
